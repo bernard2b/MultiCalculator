46b9d18ee6e08c78036d9e45ced3f226e31693ad
bernard.gyugyi <bernard.gyugyi@gmail.com>
2023-05-24 11:20:42 +0200

    add keepCalculating function

--- src/Main.java
+++ src/Main.java
@@ -4,71 +4,70 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double num1 = 0.0;
-        double num2 = 0.0;
+        double num1;
+        double num2;
         char operator;
-        double result;
+        double result = 0.0;
 
         boolean validNum1 = false;
         do {
             System.out.print("Enter the first number: ");
             if (scanner.hasNextDouble()) {
                 num1 = scanner.nextDouble();
+                result = num1; // Assign the initial number as the result
                 validNum1 = true;
-            } else {
-                scanner.next(); // Clear the invalid input from the scanner buffer
-                System.out.println("Error: Invalid number. Please try again.");
-            }
-        } while (!validNum1);
-
-        boolean validOperator = false;
-        do {
-            System.out.print("Enter an operator (+, -, *, /): ");
-            operator = scanner.next().charAt(0);
-
-            if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
-                validOperator = true;
-            } else {
-                System.out.println("Error: Invalid operator. Please try again.");
-            }
-        } while (!validOperator);
-
-        boolean validNum2 = false;
-        do {
-            System.out.print("Enter the first number: ");
-            if (scanner.hasNextDouble()) {
-                num2 = scanner.nextDouble();
-                validNum2 = true;
             } else {
                 scanner.next();
                 System.out.println("Error: Invalid number. Please try again.");
             }
-        } while (!validNum2);
+        } while (!validNum1);
 
-        switch (operator) {
-            case '+':
-                result = BasicOperators.add(num1, num2);
-                System.out.println("Result: " + result);
-                break;
-            case '-':
-                result = BasicOperators.subtract(num1, num2);
-                System.out.println("Result: " + result);
-                break;
-            case '*':
-                result = BasicOperators.multiply(num1, num2);
-                System.out.println("Result: " + result);
-                break;
-            case '/':
-                try {
-                    result = BasicOperators.divide(num1, num2);
+        boolean keepCalculating = true;
+        while (keepCalculating) {
+            boolean validOperator = false;
+            do {
+                System.out.print("Enter an operator (+, -, *, /) or '=' to show result: ");
+                operator = scanner.next().charAt(0);
+
+                if (operator == '=') {
                     System.out.println("Result: " + result);
-                } catch (ArithmeticException e) {
-                    System.out.println(e.getMessage());
+                    keepCalculating = false;
+                    break;
+                } else if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+                    System.out.print("Enter the next number: ");
+                    if (scanner.hasNextDouble()) {
+                        num2 = scanner.nextDouble();
+                        switch (operator) {
+                            case '+':
+                                result = BasicOperators.add(result, num2);
+                                break;
+                            case '-':
+                                result = BasicOperators.subtract(result, num2);
+                                break;
+                            case '*':
+                                result = BasicOperators.multiply(result, num2);
+                                break;
+                            case '/':
+                                try {
+                                    result = BasicOperators.divide(result, num2);
+                                } catch (ArithmeticException e) {
+                                    System.out.println(e.getMessage());
+                                }
+                                break;
+                            default:
+                                System.out.println("Error: Invalid operator");
+                                break;
+                        }
+                        System.out.println("Intermediate Result: " + result);
+                    } else {
+                        scanner.next();
+                        System.out.println("Error: Invalid number. Please try again.");
+                    }
+                    validOperator = true;
+                } else {
+                    System.out.println("Error: Invalid operator. Please try again.");
                 }
-                break;
-            default:
-                System.out.println("Error: Invalid operator");
-                break;
+            } while (!validOperator);
         }
     }
 }
