1c94d9e19fa05b29156cadfc50e23f37d979d29d
bernard.gyugyi <bernard.gyugyi@gmail.com>
2023-05-30 18:42:53 +0200

    add two more method for converter

--- src/ConverterCalculator.java
+++ src/ConverterCalculator.java
@@ -10,8 +10,10 @@ class ConverterCalculator extends Calculator {
 
         while (!validInput) {
             System.out.println("Converter Calculator");
-            System.out.println("1. Pounds to Kilograms");
-            System.out.println("2. Kilograms to Pounds");
+            System.out.printf("%-2s %-20s%n", "1.", "Pounds to Kilograms");
+            System.out.printf("%-2s %-20s%n", "2.", "Kilograms to Pounds");
+            System.out.printf("%-2s %-20s%n", "3.", "Grams to Ounces");
+            System.out.printf("%-2s %-20s%n", "4.", "Ounces to Grams");
             System.out.print("Enter your choice: ");
 
             try {
@@ -26,8 +28,16 @@ class ConverterCalculator extends Calculator {
                         kilogramsToPounds();
                         validInput = true;
                         break;
+                    case 3:
+                        ouncesToGrams();
+                        validInput = true;
+                        break;
+                    case 4:
+                        gramsToOunces();
+                        validInput = true;
+                        break;
                     default:
-                        System.out.println("Invalid choice. Exiting the Converter Calculator!");
+                        System.out.println("Invalid choice. Please try again.");
                         break;
                 }
             } catch (InputMismatchException e) {
@@ -59,11 +69,59 @@ class ConverterCalculator extends Calculator {
 
     public void kilogramsToPounds() {
         Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
 
-        System.out.println("Enter the weight in kilograms: ");
-        double kilogram = scanner.nextDouble();
-        double pound = kilogram * 2.20462262;
-        System.out.println("Weight in pounds: " + pound);
+        while (!validInput) {
+            try {
+                System.out.println("Enter the weight in kilograms: ");
+                double kilogram = scanner.nextDouble();
+                double pound = kilogram * 2.20462262;
+                System.out.println("Weight in pounds: " + pound);
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid weight in kilograms. ");
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+    public void ouncesToGrams() {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.println("Enter the weight in ounces: ");
+                double ounce = scanner.nextDouble();
+                double gram = ounce * 28.3495;
+                System.out.println("Weight in grams: " + gram);
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid weight in grams. ");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public void gramsToOunces() {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.println("Enter the weight in grams: ");
+                double gram = scanner.nextDouble();
+                double ounce = gram * 0.035274;
+                System.out.println("Weight in ounces: " + ounce);
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid weight in ounces. ");
+                scanner.nextLine();
+            }
+        }
     }
 
 
